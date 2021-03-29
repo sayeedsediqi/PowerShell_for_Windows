f84471f5ba0087764c5f85e16c3feae88dd14c98
Sayeed <60190007+sayeedsediqi@users.noreply.github.com>
2021-03-28 20:59:18 -0400

    Create List_of_All_Servers_with_OS_type_PowerShell_Script.ps1

---
+++ List_of_All_Servers_with_OS_type_PowerShell_Script.ps1
@@ -0,0 +1,14 @@
+﻿$computers=import-csv C:\Temp11\servers.csv
+foreach($computer in $computers){
+$info=get-adcomputer $computer.Servername -Properties * |select Name,application, businesscatagory, department, description, environment, OperatingSystem, Managedby 
+$name = $info.name
+$application = $info.application
+$environment = $info.environment
+$dept = $info.department
+$ContactEmail = $info.Managedby
+$bcat = $info.businesscatagory
+$ostype= $info.OperatingSystem
+$newline="$name,$application,$bcat,$dept,$environment,$ostype,$ContactEmail"
+$newline | add-content "c:\temp11\testblabla18.csv"
+
+}
