802125e7f3df692d618c32c1a2c10d59445ae7b3
Sayeed Mohammad <60190007+sayeedsediqi@users.noreply.github.com>
2021-06-12 11:52:07 -0400

    Add files via upload

---
+++ How to Check Active Directory Replication.ps1
@@ -0,0 +1,21 @@
+﻿# Repadmin: How to Check Active Directory Replication
+
+#Export the replication partner as well as the replication status
+repadmin /showrepl |out-file c:\temp\showrepl.txt
+
+#Export replication summray
+repadmin /replsummary |out-file c:\temp\showreplSummary.txt
+
+#List the Topology information
+repadmin /bridgeheads * /verbose
+
+#Inter Site Topology Generator Report
+repadmin /istg * /verbose
+
+#Displays calls that have not yet been answered
+repadmin /bridgeheads  |out-file c:\temp\bridgeheads_topology1.txt
+
+
+
+
+
